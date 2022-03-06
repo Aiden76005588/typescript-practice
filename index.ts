d535ad534092d6b367d6ef616ce0dfc7543ee5f7
Aiden <fsd76005588@gmail.com>
2022-03-06 21:57:32 +0900

    types

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "lodo",
+  age: 22,
+  gender: "male",
+};
+
+const name = "Nico",
+  age = 24,
+  gender = "male";
+
+const sayHi = (person) => {
+  console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+};
+
+console.log(sayHi(person));
+export {};
